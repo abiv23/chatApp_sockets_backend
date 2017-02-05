fc37367d6cb63b0c62ccebcce150b5bd201b568a
Mike Dowd <mikedowd1972@gmail.com>
2017-02-04 20:31:36 -0700

    awesome stuff

--- index.js
+++ index.js
@@ -6,44 +6,35 @@ client.on('connection', function(socket) {
     console.log('connected');
 
     socket.on('first-contact', function(data) {
-        //Call the express server, it looks for a cookie in the header, if its not there it creates a new
-        //account then it calls the db, then it gets the messages from the db, it
-        //then sends the meaasges back here then I send them to the client
-        //If there is a cookie to give to a new user I would send that along with the messages
+      console.log('im here');
+        //Call the Laravel server, it looks for a cookie in the header,
+        //If it is there it gets the name associated with the cookie
+        //if its not there it creates a cookie and unique name
+        //then it gets the messages from the db, it then sends the messages back here then I send them to the client
+        //If there is a cookie to give to a new user I would send that along with the messages (and the name too)
         let obj = {
             messageRay: [{
-                name: "bob",
-                message: "Bob is awesomey"
+                name: "mob",
+                message: "Bob0 is awesomey"
             }, {
-                name: "Joe",
-                message: "Joe is awesomer"
-            }]
+                name: "Moe",
+                message: "Mary is my friend"
+            }],
+            name: "MJB",//The name comes from Laravel Server
+            isCookie : true //This is because there is gonna be a cookie set right here, I just dont know how to do that
         };
-        client.emit('message-list', obj)
+        client.emit('message-list-and-name', obj)
     });
 
     socket.on('send-message', function(data) {
         console.log(data);
-        //I could send this to the DB directly from here, but I think the Laravel back end wants to do it.
-
-        //maybe one of the key value pairs in the object is the website, this way the clients could all decide for them selves if they want to display it
-
-        //Or I coud make a string out of the data.url - Duh, thats the best
+        //The trick here is that the data.url is the name of the event
+        //This makes it so tht only the people at that website recieve it
         socket.emit(data.url, data);
     });
 
     socket.on('disconnect', function() {
         console.log(socket.id);
     });
-
-    // function getSocket(id) {
-    //   let mySock = sockets[0];
-    //   sockets.forEach(function(sock) {
-    //     if(sock.id === id) {
-    //       mySock = sock;
-    //     }
-    //   });
-    //   return mySock;
-    // }
 
 });
