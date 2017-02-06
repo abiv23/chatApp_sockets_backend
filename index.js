43534d7dd18d6b87b85a5b191d48c2aac6c867b2
Mike Dowd <mikedowd1972@gmail.com>
2017-02-06 11:50:04 -0700

    worked on chat feature

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const socket = require('socket.io');
 const port = process.env.PORT || 3000;
 const client = require('socket.io').listen(port).sockets;
 
+let messages = [{
+    name: "mob",
+    message: "Bob0 is awesomey"
+}, {
+    name: "Moe",
+    message: "Mary is my friend"
+}]
+
 client.on('connection', function(socket) {
-    console.log('connected');
+  console.log('connected');
 
     socket.on('first-contact', function(data) {
       console.log('im here');
@@ -13,24 +21,20 @@ client.on('connection', function(socket) {
         //then it gets the messages from the db, it then sends the messages back here then I send them to the client
         //If there is a cookie to give to a new user I would send that along with the messages (and the name too)
         let obj = {
-            messageRay: [{
-                name: "mob",
-                message: "Bob0 is awesomey"
-            }, {
-                name: "Moe",
-                message: "Mary is my friend"
-            }],
+            messageRay: messages,
             name: "MJB",//The name comes from Laravel Server
             isCookie : true //This is because there is gonna be a cookie set right here, I just dont know how to do that
         };
-        client.emit('message-list-and-name', obj)
+        socket.emit('message-list-and-name', obj)
     });
 
     socket.on('send-message', function(data) {
         console.log(data);
         //The trick here is that the data.url is the name of the event
         //This makes it so tht only the people at that website recieve it
-        socket.emit(data.url, data);
+        messages.push(data);
+        console.log(messages);
+        client.emit('www.google.com', data);
     });
 
     socket.on('disconnect', function() {
